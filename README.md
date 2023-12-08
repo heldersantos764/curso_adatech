80d99cde0b2a3efb6d7830fce1e336f0cb048156
heldersantos764 <heldersantos764@gmail.com>
2023-12-08 20:09:42 -0300

    Ciração do repositório

---
+++ README.md
@@ -0,0 +1,3 @@
+# Repositório para controle do curso Vem Ser Tech - AdaTech
+
+Atividade registradas apartir do módulo 03
---
+++ modulo03/projeto_final/assets/js/WordQuest.js
@@ -0,0 +1,8 @@
+class WordQuest{
+    constructor(username){
+        this.usename = username
+        this.level = 1
+        this.score = 0
+        this.continue = true
+    }
+}
